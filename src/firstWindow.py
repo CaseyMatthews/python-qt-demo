69bddbd3a70fb31440007ae985a6b9f6088d3fe1
CaseyMatthews <casey-matthews@live.com>
2022-02-26 11:17:17 -0700

    init virtual env, install pyside2, and begin code work

---
+++ src/firstWindow.py
@@ -0,0 +1,26 @@
+# Following this tutorial: https://www.pythonguis.com/tutorials/pyside-creating-your-first-window/
+
+from PySide2.QtCore import QSize, Qt
+from PySide2.QtWidgets import QApplication, QMainWindow, QPushButton
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+        button = QPushButton("Press Me!")
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_clicked)
+
+        self.setCentralWidget(button)
+
+    def the_button_was_clicked(self):
+        print("Clicked!!!")
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec_()
